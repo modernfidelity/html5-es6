5dbeb3f9776787360d1a2fcb197160fcbbed9d09
modernfidelity <mark@modernfidelity.co.uk>
2015-03-21 19:01:03 +0000

    first commit

---
+++ js/src/main.js
@@ -0,0 +1,20 @@
+/***
+ *
+ * Main JS
+ *
+ * @type {number}
+ */
+
+
+import Monkey from "./monkey-api"
+
+console.log("es6 loaded");
+
+//var bob = new Monkey({
+//    name: 'bob'
+//});
+
+
+Monkey.load();
+
+export default {}
---
+++ js/src/monkey-api.js
@@ -0,0 +1,20 @@
+
+
+class Monkey {
+    //constructor(properties) {
+    //    this.properties = properties;
+    //}
+    //
+    //toObject() {
+    //    return this.properties;
+    //}
+
+    load() {
+        console.log("I am bob");
+    }
+
+}
+
+export default new Monkey();
+
+
